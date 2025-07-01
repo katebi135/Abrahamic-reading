6a65bb25e1137e992d5bce87a4291844b682a5d0
katebi135 <katebi135@gmail.com>
2025-06-30 22:00:19 -0400

    Add files via upload

--- compatibility_module.py
+++ compatibility_module.py
@@ -1,1 +1,5 @@
-# Compatibility logic based on names and dates
+
+def basic_name_numerology(name):
+    # Very basic Abjad-style numerology (sum of letter Unicode values)
+    total = sum(ord(c) for c in name if c.isalpha())
+    return total
